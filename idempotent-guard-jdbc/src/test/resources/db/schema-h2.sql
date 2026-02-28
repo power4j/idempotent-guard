5324d983195ef1f7c0420307ecda2cd96857d840
cj <jclazz@outlook.com>
2026-03-01 00:05:21 +0800

    style: 统一 schema-h2.sql 关键字大小写和列对齐风格

--- idempotent-guard-jdbc/src/test/resources/db/schema-h2.sql
+++ idempotent-guard-jdbc/src/test/resources/db/schema-h2.sql
@@ -1,9 +1,10 @@
-CREATE TABLE lock_guard (
-    id              VARCHAR(128)  NOT NULL,
-    start_time_utc  TIMESTAMP     NOT NULL,
-    expire_time_utc TIMESTAMP     NOT NULL,
-    hold_by         VARCHAR(255)  NOT NULL,
-    hold_hint       VARCHAR(255)  NULL,
-    hold_token      VARCHAR(36)   NOT NULL DEFAULT '',
-    CONSTRAINT PK_LOCK_GUARD PRIMARY KEY (id)
+
+create table lock_guard (
+    id                   VARCHAR(128)     not null,
+    start_time_utc       TIMESTAMP        not null,
+    expire_time_utc      TIMESTAMP        not null,
+    hold_by              VARCHAR(255)     not null,
+    hold_hint            VARCHAR(255)     null,
+    hold_token           VARCHAR(36)      not null default '',
+    constraint PK_LOCK_GUARD primary key (id)
 );
